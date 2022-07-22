1cba5129a84c87f23d1ad638b3a9672045b84ecf
mina1961 <103601185+mina1961@users.noreply.github.com>
2022-07-22 06:40:30 +0300

    Update browser.js

--- public/browser.js
+++ public/browser.js
@@ -33,8 +33,8 @@ document.getElementById("create-form").addEventListener("submit", function(e) {
 document.addEventListener("click", function(e) {
     //Delete Feature
     if (e.target.classList.contains("delete-me")) {
-        if (confirm("Do you really want to delete yhis item permanently?")) {
-            axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(function() {
+        if (confirm("Do you really want to delete this item permanently?")) {
+            axios.post('/delete-item', {id: e.target.getAttribute("data-id")}).then(function () {
             e.target.parentElement.parentElement.remove()
         }).catch(function() {
             console.log("Please try again later.")
@@ -44,7 +44,7 @@ document.addEventListener("click", function(e) {
     
     //Update Feature
     if (e.target.classList.contains("edit-me")) {
-        let userInput = prompt("Enter your dsired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+        let userInput = prompt("Enter your desired new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
             axios.post('/update-item', {text: userInput, id: e.target.getAttribute("data-id")}).then(function() {
             e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
